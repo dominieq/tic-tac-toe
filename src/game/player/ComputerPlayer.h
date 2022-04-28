a7b80d4ff13595e8fab0dc7891c090b645c3d1b7
Dominik Szmyt <domino.onet.pl@op.pl>
2022-04-28 13:33:48 +0200

    Add documentation for Player classes

--- src/game/player/ComputerPlayer.h
+++ src/game/player/ComputerPlayer.h
@@ -3,14 +3,41 @@
 
 #include "Player.h"
 
+/**
+ * Represents a "Computer" player that responds to opponent's actions <br/>
+ * by analyzing a tic-tac-toe board with the
+ * <a href="https://en.wikipedia.org/wiki/Minimax" target="_blank">Minmax algorithm</a>.
+ *
+ * @author Dominik Szmyt
+ * @since 1.0.0
+ */
 class ComputerPlayer : public Player {
 
 public:
+	/**
+	 * Create an instance with player's name and sign (X or O).
+	 *
+	 * @param InName The player's name.
+	 * @param InSign The player's sign (X or O).
+	 */
 	ComputerPlayer(const std::string& InName, BoardContent InSign);
 
+	/**
+	 * Performs a move by executing the Minmax algorithm.
+	 *
+	 * @param InBoard The current state of a tic-tac-toe board.
+	 */
 	void PerformMove(Board*& InBoard) override;
 
 protected:
+	/**
+	 * Carries out the actual Minmax algorithm.
+	 *
+	 * @param InBoard The current state of a tic-tac-toe board.
+	 * @param Depth   The current level of a minmax tree.
+	 * @param IsMax   Whether we are estimating for a "max" player.
+	 * @return The minmax evaluation for the given arguments.
+	 */
 	int Minmax(Board*& InBoard, int Depth, bool IsMax);
 };
 
--- src/game/player/HumanPlayer.h
+++ src/game/player/HumanPlayer.h
@@ -3,11 +3,28 @@
 
 #include "Player.h"
 
+/**
+ * Represents a player that performs moves via the standard input (console).
+ *
+ * @author Dominik Szmyt
+ * @since 1.0.0
+ */
 class HumanPlayer : public Player {
 
 public:
+	/**
+	 * Create an instance with player's name and sign (X or O).
+	 *
+	 * @param InName The player's name.
+	 * @param InSign The player's sign (X or O).
+	 */
 	HumanPlayer(const std::string& InName, BoardContent InSign);
 
+	/**
+	 * Performs a move by selecting a row and column via the standard input (console).
+	 *
+	 * @param InBoard The current state of a tic-tac-toe board.
+	 */
 	void PerformMove(Board*& InBoard) override;
 };
 
